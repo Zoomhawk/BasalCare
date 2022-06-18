511b129dd8ff438e94fc1aaa2708027e1b17827b
Somesh Singh <someshsingh298@gmail.com>
2022-06-18 15:18:16 +0530

    added navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,13 +12,19 @@ function NavBar() {
     }
   };
   return (
-    <div>
-      <div>BasalCare</div>
-      {currentUser ? (
-        <div onClick={() => logout()}>logout</div>
-      ) : (
-        <div onClick={loginHandler}>Login</div>
-      )}
+    <div className="bg-blue-50 h-16 grid grid-cols-2">
+      <div className="p-2 pl-8">
+        <img src="/logo.png" className="h-12 w-24" />
+      </div>
+      <div className="flex justify-end items-center pr-20">
+        <div className="h-12 w-24 border-2 flex justify-center items-center rounded-lg cursor-pointer">
+          {currentUser ? (
+            <div onClick={() => logout()}>logout</div>
+          ) : (
+            <div onClick={loginHandler}>Login</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
