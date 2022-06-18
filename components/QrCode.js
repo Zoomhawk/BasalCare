6d3c8825fe9834c915ab8e5a29238428ed27f437
Somesh Singh <someshsingh298@gmail.com>
2022-06-18 12:52:39 +0530

    added component folder

---
+++ components/QrCode.js
@@ -0,0 +1,11 @@
+import React from "react";
+import QRCode from "react-qr-code";
+function QrCode() {
+  return (
+    <div>
+      <QRCode value="hey" />
+    </div>
+  );
+}
+
+export default QrCode;
